2334c7d3076dcab779796c20cadc7cf020bbbaeb
la <alvispets@gmail.com>
2022-08-13 00:43:02 +0800

    ..

---
+++ compositonApi/src/components/viewShow.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>计数器<span class="difStyle">{{count}}</span></div>
+</template>
+
+<script>
+export default {
+  name: "viewShow",
+  props:['count'],
+  setup(){
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- compositonApi/src/views/HomeView.vue
+++ compositonApi/src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <viewShow :count="count"/>
+    <viewControler @parentIncrement="increment($event)" />
   </main>
 </template>
+<script >
+import {ref} from "vue";
+import viewShow from "../components/viewShow.vue";
+import viewControler from "../components/viewControler.vue";
+  export default {
+    components:{
+      viewShow,
+      viewControler
+    },
+    setup (){
+      let count=ref(1000);
+      const increment=(num)=>{
+        count.value+=num;
+      }
+      return{
+        count,
+        increment
+      }
+
+    }
+  }
+</script>
+<style scoped>
+  .difStyle{
+    font-size: 18px;
+    color: lightgreen;
+  }
+</style>
