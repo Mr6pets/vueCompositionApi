98c1982b28e18fc0acf5cfcd834d9e8a3f97bef4
la <alvispets@gmail.com>
2022-09-06 13:37:20 +0800

    .....

---
+++ compositonApi/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="py-5 text-center">
+    <img height="150" src="@/assets/logo.png" alt="" />
+    <h2>我是标题</h2>
+    <p class="lead">梦想还是要有的，万一实现了嫩</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"header",
+
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
